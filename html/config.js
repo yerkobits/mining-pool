cd123c6aca57c06214b0457229ed684c2c57e408
ParsiCoin <parsicoin@gmail.com>
2019-03-18 03:06:50 +0330

    initial

---
+++ html/config.js
@@ -0,0 +1,13 @@
+var api = "http://pool.parsicoin.net:8117";
+
+var email = "contact@parsicoin.net";
+var telegram = "https://t.me/parsicoin";
+var discord = "https://discord.gg/c7qmFNh";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://explorer.parsicoin.net/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.parsicoin.net/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
